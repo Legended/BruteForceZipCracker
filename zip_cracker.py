d1c72c9f2a3661a786aa0ec6cb836456e0a29388
Legended <jollyrogers1503@gmail.com>
2019-07-26 15:49:41 -0700

    Update zip_cracker.py

--- zip_cracker.py
+++ zip_cracker.py
@@ -5,13 +5,24 @@ import zipfile, zlib
 
 
 class BruteZip:
+
     def __init__(self, src='', chars=string.printable.strip(), min_length=1, max_length=None):
+        """This script cracks a zip file via brute force. This is intended for educational purposes only.
+
+        :param src: Path of zip file.
+        :param chars: Character to scan for.
+        :param min_length: Minimum length of password.
+        :param max_length: Maximum length of password
+        """
+        
         self.src = src
         self.chars = chars
         self.min_length = min_length
         self.max_length = max_length
 
     def crack_zip(self):
+        """Iterates through each possible combination and prints the results of each scan."""
+        
         start = time()
         count = 1
         minimum = self.min_length
@@ -28,24 +39,9 @@ class BruteZip:
                         count += 1
                 self.min_length += 1
 
-    def crack_zip_reversed(self):
-        start = time()
-        count = 1
-        minimum = self.min_length
-        with zipfile.ZipFile(self.src, 'r') as zf:
-            while True:
-                self.check_length_queries()
-                for pwd in itertools.product(reversed(self.chars), repeat=self.max_length):
-                    try:
-                        zf.extractall(pwd=bytes(''.join(pwd), encoding='utf-8'))
-                        return self.success_message(count, minimum, pwd, start)
-                    except (RuntimeError, zlib.error, zipfile.BadZipFile):
-                        print(f"[{count}] [-] Password Failed: {''.join(pwd)} | "
-                              f"Elapsed Time: {timedelta(seconds=time() - start)}")
-                        count += 1
-                self.max_length -= 1
-
     def check_length_queries(self):
+        """Checks the params for 'max_length' and 'min_length'. 'min_length cannot be greater than 'max_length'."""
+        
         if self.max_length is not None:
             if self.min_length > self.max_length:
                 raise ValueError("'min_length' cannot be greater than 'max_length'")
@@ -54,6 +50,8 @@ class BruteZip:
             raise SystemExit
 
     def total_scan_results(self, minimum):
+        """Calculates the possible amount of combinations it would take to crack a password."""
+        
         exponent = minimum
         results = []
         while exponent <= self.max_length:
@@ -62,6 +60,8 @@ class BruteZip:
         return sum(results)
 
     def success_message(self, count, minimum, pwd, start):
+        """Message printed when the password has successfully been cracked."""
+        
         if self.max_length is not None:
             fmt = "\n+{}+\n|{:^88}|\n|{:^88}|\n|{:^88}|\n+{}+"
             return print(fmt.format(
